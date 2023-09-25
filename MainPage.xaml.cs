d9fcc5d185720af4a0069cb71108f01be3017639
EmG <120766457+Emcoding15@users.noreply.github.com>
2023-09-25 08:14:26 +0800

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace MAUIApp22;
+
+public partial class MainPage : ContentPage
+{
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnExpectationClicked(object sender, EventArgs e)
+    {
+        ExpectationImgBtn.Source = "app21.png";
+        ExpectationLabel.Text = "Reality";
+    }
+
+    private void OnExpectationClicked2(object sender, EventArgs e)
+    {
+        ExpectationImgBtn2.Source = "app21.png";
+        ExpectationLabel2.Text = "Reality";
+    }
+}
